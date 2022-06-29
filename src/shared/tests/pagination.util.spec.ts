f37a5ab47793c7e3af4c43a267a272570f773fc8
Darvand <david.montoya@rootstrap.com>
2022-06-29 16:23:49 -0500

    Add util method to calculate take and skip values

--- src/shared/tests/pagination.util.spec.ts
+++ src/shared/tests/pagination.util.spec.ts
@@ -1,15 +1,29 @@
 import { createFakeProductEntityArray } from '@main/products/tests/fakes/products.fake';
 import { PaginationUtils } from '../utils/pagination.util';
+import { createFakePaginationQueryDTO } from './fakes/pagination.fake';
 
 describe('PaginationUtils Unit Test', () => {
-  it('should wrap the products on a pagination interface', () => {
-    const products = createFakeProductEntityArray(10);
-    const response = PaginationUtils.createPagination(products, 100, 2, 10);
+  describe('describePagination', () => {
+    it('should wrap the products on a pagination interface', () => {
+      const products = createFakeProductEntityArray(10);
+      const response = PaginationUtils.createPagination(products, 100, 2, 10);
 
-    expect(response).toBeDefined();
-    expect(response.actualPage).toEqual(2);
-    expect(response.results.length).toEqual(products.length);
-    expect(response.totalAmount).toEqual(100);
-    expect(response.totalPages).toEqual(10);
+      expect(response).toBeDefined();
+      expect(response.actualPage).toEqual(2);
+      expect(response.results.length).toEqual(products.length);
+      expect(response.totalAmount).toEqual(100);
+      expect(response.totalPages).toEqual(10);
+    });
+  });
+
+  describe('createFindOptions', () => {
+    it('should calculate take and skip values', () => {
+      const query = createFakePaginationQueryDTO(10, 4);
+      const response = PaginationUtils.createFindOptions(query);
+
+      expect(response).toBeDefined();
+      expect(response.take).toEqual(query.limit);
+      expect(response.skip).toEqual(30);
+    });
   });
 });
